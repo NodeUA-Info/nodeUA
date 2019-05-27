26e4060ca335654e7315ec069ee16aba65d13cb9
Alina Kizilova <alinakzilova@gmail.com>
2019-05-27 23:13:33 +0300

    added routing

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
 import App from "./components/App";
+import SignIn from "./components/Auth/SignIn";
+import SignUp from "./components/Auth/SignUp";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const Root = () => (
+  <Router>
+    <Switch>
+      <Route path="/" exact component={App} />
+      <Route path="/signin" component={SignIn} />
+      <Route path="/signup" component={SignUp} />
+      <Redirect to="/" />
+    </Switch>
+  </Router>
+);
+
+ReactDOM.render(<Root />, document.getElementById("root"));
